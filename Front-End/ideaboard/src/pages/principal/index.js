ed927367b662269a6ebfadbb61219a2f46373d36
TheLOL1 <gabriellimati4@gmail.com>
2020-03-20 06:34:42 -0300

    Tela principal em andamento

--- Front-End/ideaboard/src/pages/principal/index.js
+++ Front-End/ideaboard/src/pages/principal/index.js
@@ -1,7 +1,8 @@
-import React, { useEffect, Component } from "react"; //importa modulos do react
-import {H1, Container} from "./styles"; //importa os estilos definidos em styles.js
+import React, { useEffect, useState } from "react"; //importa modulos do react
+import {H1, Container, ContainerForm, Form, Button, ContainerButton, DeleteButton} from "./styles"; //importa os estilos definidos em styles.js
 import Api from "../../services/Api"; //importa o axios definido em services
 import { deslogar, getToken } from "../../services/auth"; //importa o método de logar definido em auth.js
+import Select from 'react-select'; //importa o select do react
 
 /**
  * Exporta a pagina principal da aplicação.
@@ -9,20 +10,109 @@ import { deslogar, getToken } from "../../services/auth"; //importa o método de
 
 export default function Principal()
 {
+    const [ideias,setIdeias] = useState([]); //define o estado ideia juntamente com seu metodo de set, e seta o estado inicial como array vazio
+    const user_id = getToken() //retorna o id do usúario logado
+
+    /**
+     * Array com as opções do select.
+     */
+
+    const opcoesOrdenar =
+    [
+        {
+            value: "maisrecentes",
+            label: "Mais recentes"
+        },
+        {
+            value: "maisantigos",
+            label: "Mais antigos"
+        }
+    ]
+
+    /**
+     * Realiza um get na API que irá retornar as ideias vinculadas ao usúario logado e seta o estado de ideias, se o usúario não tiver nenhuma ideia vinculada é chamado o método inserirIdeia().
+     */
+
+    async function carregarIdeias()
+    {
+        await Api.get("/listIdeasUser",{
+            headers: {user_id}
+        }).then(function (resposta)
+        {
+            setIdeias(resposta.data);
+            if (resposta.data.length === 0)
+            {
+                inserirIdeia();
+            }
+        });
+    }
+
+    /**
+     * Ao carregar a pagina chama o carregarIdeias() e se o usúario não possuir nehuma ideia inserida no banco de dados chama o inserirIdeia().
+     */
+
     useEffect(() =>
     {
-        async function carregarIdeias()
-        {
-            const resposta = await Api.get("/listIdeasUser",{
-                headers: {user_id: getToken()}
-            });
-            console.log(resposta.data);
-        }
         carregarIdeias();
     },[]);
+
+    /**
+     * Realiza um post na API para inserir uma nova ideia e chama o carregar ideias para pode ser criado um novo form. 
+     */
+
+    async function inserirIdeia()
+    {
+        await Api.post("/insertIdea",{
+            titulo: "",
+            descricao: ""
+        },{
+            headers: {user_id}
+        });
+        carregarIdeias();
+    }
+
+    /**
+     * Realiza um delete na API para remover uma ideia a partir do seu id, filtra o array de ideias para atualizar a interface e previne o botão de recarregar a pagina.
+     */
+
+    async function deletarIdeia(e,idea_id)
+    {
+        e.preventDefault();
+        await Api.delete("/removeIdea",{
+            headers: {idea_id}
+        }).then(function ()
+        {
+            setIdeias(ideias.filter(ideia => ideia._id !== idea_id));  
+        }); 
+    }
+    
+
+    /**
+     * Define o HTML da pagina.
+     */
+
+
     return (
+        <>
             <Container>
-            <H1>Quadro de Ideias</H1>
+                <H1>Quadro de Ideias</H1>
             </Container>
+            <ContainerButton>
+                <Button type="submit" onClick={() => inserirIdeia()}>Nova Ideia</Button>
+                <Select options={opcoesOrdenar} defaultValue={opcoesOrdenar[0]} />
+            </ContainerButton>
+            <ContainerForm>
+                {ideias.map(ideia =>
+                            (
+                                <Form key={ideia._id}>
+                                <DeleteButton onClick={e => deletarIdeia(e,ideia._id)}></DeleteButton>
+                                <input type="text" placeholder="Título da Ideia" defaultValue={ideia.titulo}/>
+                                <textarea id="descricao" type="text" placeholder="Descreva a Ideia" defaultValue={ideia.descricao}/>
+                                </Form>
+                            )
+                        )
+                }
+            </ContainerForm>
+        </>
     );
 }
--- Front-End/ideaboard/src/pages/principal/styles.js
+++ Front-End/ideaboard/src/pages/principal/styles.js
@@ -1,4 +1,9 @@
 import styled from "styled-components"; //importa os modulos do styled componentes
+import deletar from "../../assets/delete.png";
+
+/**
+ * Estiliza a pagina principal da aplicação que é utilizado em index.js
+ */
 
 export const H1 = styled.h1`
     color: white;
@@ -9,4 +14,79 @@ export const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+`;
+
+export const ContainerForm = styled.div`
+    display: flex;
+    width: 90%;
+    margin-top: 10px;
+    padding: 40px;
+    border-radius: 15px;
+`;
+
+export const Form = styled.form`
+    margin-right:30px;
+    width: 18%;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    margin-bottom: 40px;
+    border-radius: 15px;
+    background: yellow;
+    input
+    {
+        background: none;
+        outline:none;
+        font-size:20px;
+        border: 0;
+        padding:10px;
+    }
+    input:focus
+    {
+        border: solid 1px blue;
+        border-radius: 15px;
+    }
+    #descricao
+    {
+        background: none;
+        outline:none;
+        font-size:20px;
+        border: 0;
+        padding:10px;
+        resize: none;
+        height: 160px;
+    }
+    #descricao:focus
+    {
+        border: solid 1px blue;
+        border-radius: 15px;
+    }
+`;
+
+export const Button = styled.button`
+    color: #fff;
+    font-size: 16px;
+    background:  #253c51;
+    height: 30px;
+    border: 0;
+    border-radius: 5px;
+    width: 10%;
+    margin-right: 30px;
+`;
+
+export const ContainerButton = styled.div`
+    margin-top: 18px;
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    margin-left: 50px;
+`;
+
+export const DeleteButton = styled.button`
+    background-image: url(${deletar});
+    height: 32px;
+    width: 32px;
+    margin-left: 150px;
+    margin-top: 10px;
 `;
